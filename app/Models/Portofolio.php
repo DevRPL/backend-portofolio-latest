4f0328be33b56b3527ce8e3281171646b797bbed
Muhamad Jamil Firdaus <muhamadjamil668@gmail.com>
2024-10-24 17:41:47 +0700

    migration table

--- app/Models/Portofolio.php
+++ app/Models/Portofolio.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portofolio extends Model
 {
-    protected $table = 'portopolioes';
+    protected $table = 'portofolioes';
 
     protected $guarded = [];
 }
